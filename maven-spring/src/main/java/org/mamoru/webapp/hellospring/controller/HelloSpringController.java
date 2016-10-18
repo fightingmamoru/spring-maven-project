0cb0a893c87caf2ecbd7f5cf3b50044c68f74fff
fightingmamoru <fightingmamoru@gmail.com>
2016-10-18 14:56:14 +0900

    04.Add log4j2 & slf4j for logging

--- maven-spring/src/main/java/org/mamoru/webapp/hellospring/controller/HelloSpringController.java
+++ maven-spring/src/main/java/org/mamoru/webapp/hellospring/controller/HelloSpringController.java
@@ -1,5 +1,7 @@
 package org.mamoru.webapp.hellospring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,15 +12,26 @@ public class HelloSpringController
 {
 	private static final String JSP_PATH = "hellospring/";
 
+	// Logger
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloSpringController.class);
+
 	@RequestMapping (value = "/helloSpring")
 	public ModelAndView helloSpring()
 	{
+		// Start Log
+		String startLogMessage = "START Log";
+		LOGGER.debug("HelloSpring Log - {}", startLogMessage);
+
 		ModelAndView mav = new ModelAndView();
 
 		mav.setViewName(JSP_PATH + "helloSpring");
 
 		mav.addObject("title", "Spring Web MVC - HelloSprig");
 		mav.addObject("message", "Hello Spring!");
+
+		// End Log
+		String endLogMessage = "END Log";
+		LOGGER.debug("HelloSpring Log - {}", endLogMessage);
 
 		return mav;
 	}

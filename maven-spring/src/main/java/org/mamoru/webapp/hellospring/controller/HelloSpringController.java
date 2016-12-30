b11bc9dcf5885be6e03bb2a78cecb00e5ca553a2
fightingmamoru <ma131026mo*>
2016-12-31 00:58:21 +0900

    add jquery dynamic form.

--- maven-spring/src/main/java/org/mamoru/webapp/hellospring/controller/HelloSpringController.java
+++ maven-spring/src/main/java/org/mamoru/webapp/hellospring/controller/HelloSpringController.java
@@ -3,8 +3,12 @@ package org.mamoru.webapp.hellospring.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping (value = "/hellospring")
@@ -32,6 +36,24 @@ public class HelloSpringController
 		// End Log
 		String endLogMessage = "END Log";
 		LOGGER.debug("HelloSpring Log - {}", endLogMessage);
+
+		return mav;
+	}
+
+	@RequestMapping (value = "/paramTest/{type}")
+	public ModelAndView paramTest(@PathVariable("type") String type,
+								  @RequestParam(value = "username") String username,
+								  HttpServletRequest request)
+	{
+		ModelAndView mav = new ModelAndView();
+
+		System.out.println("[Parameter Check] type : " + type);
+		System.out.println("[Parameter Check] username : " + username);
+
+		mav.setViewName(JSP_PATH + "paramtest");
+
+		mav.addObject("type", type);
+		mav.addObject("username", username);
 
 		return mav;
 	}
---
+++ maven-spring/src/main/webapp/view/hellospring/paramtest.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+	<head>
+		<title>Parameter Test</title>
+	</head>
+
+	<body>
+		<h2>Parameter Test</h2>
+		<h4>- @PathVariable and @RequestParam Test</h4>
+
+		<br/>
+
+		@PathVariable : ${type}
+
+		<br/><br/>
+
+		@RequestParam : ${username}
+	</body>
+</html>
